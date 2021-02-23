71ea3f3343696a97c98f3b73562b52846c6a4950
Johann Taylor <j.taylor1343@gmail.com>
2021-02-23 02:29:24 -0500

    Add ability to show and delete candidates

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
-
 const PORT = process.env.PORT || 3001;
 const app = express();
+const sqlite3 = require('sqlite3').verbose();
+
+// Connect to database
+const db = new sqlite3.Database('./db/election.db', err => {
+    if (err) {
+        return console.log(err.message);
+    }
+    console.log('Connected to the election database.');
+})
+
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
@@ -13,12 +22,81 @@ app.get('/', (req, res) => {
     });
 });
 
-// Default response for any other request (Not Found)
+//db object using all method to show rows matching query
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+        if(err){
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: "success",
+            data: rows
+        });
+    });
+});
+
+
+// GET a single candidate
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates
+                WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+        if(err){
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: "success",
+            data: row
+        });
+    });
+});
+
+
+// Delete a candidate
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function(err, row) { //ES5 function to use statement object's 'changes' property (this)
+        if(err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: "successfully deleted",
+            changes: this.changes
+        });
+    });
+});
+
+// Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//             VALUES (?,?,?,?)`;
+
+// const params = [1, 'Ronald', 'Firbank', 1];
+
+// ES5 function, not arrow function, to us this
+// db.run(sql, params, function(err, result) {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(result, this.lastID);
+// });
+
+
+// Default response for any other request (Not Found) Catch all
 app.use((req,res) => {
     res.status(404).end();
 });
 
-// Start server
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+// Start server after connecting to the DB
+db.on('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
 });
+
